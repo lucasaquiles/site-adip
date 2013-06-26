d2f3f3e290d9f87c8f81019548fba7b3f7d81def
lucasaquiles <lucasaquiles@gmail.com>
2013-06-26 14:06:42 -0300

    ajusts

--- wp-content/themes/twentytwelve/index.php
+++ wp-content/themes/twentytwelve/index.php
@@ -18,12 +18,12 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-		<?php if ( have_posts() ) : ?>
+		<?php $i = 0; if ( have_posts())  : ?>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
+			<?php while ( have_posts() && i == 0) : the_post(); ?>
+				<?php get_template_part( 'content-home', get_post_format() ); ?>
+			<?php $i++;endwhile; ?>
 
 			<?php twentytwelve_content_nav( 'nav-below' ); ?>
 
